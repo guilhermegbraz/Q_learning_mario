eaf19cd0bd80e5ecef7a75fe4ba6c805b62b5ad5
Guilherme <guilherme.g.braz@outlook.com>
2024-12-06 00:11:35 -0300

    Adição método para quando estiver no ar, avançar até o mario voltar ao chão para então pontuar a ultima jogada (pulo)

--- q_learning/QLearningMario.py
+++ q_learning/QLearningMario.py
@@ -17,14 +17,14 @@ class QLearningMario:
                 max_steps_per_episode: int, acoes: List[AcaoMario], compute_reward):
         radius = 6
         n_actions = len(acoes)
-        # env = retro.make(game='SuperMarioWorld-Snes', state=self.fase, players=1)
-        env = retro.make(game='SuperMarioWorld-Snes', state='YoshiIsland1', players=1)
+        env = retro.make(game='SuperMarioWorld-Snes', state=self.fase, players=1)
+        maximo_passos = 0
+        pos_x = 0
         for episode in range(n_episodes):
             print(f"Iniciando episódio {episode + 1}/{n_episodes}...")
             env.reset()
             ram = self.getRam(env)
             state, x, y = getState(ram, radius=radius)
-            print(state)
             for step in range(max_steps_per_episode):
 
                 if not q_table.estado_ja_existe(ram, step):
@@ -41,16 +41,21 @@ class QLearningMario:
                 else:
                     indice_acao = acoes.index(q_table.get_acao_maxima(ram, step))
 
-                print(f"posicao mario: ({x}, {y})")
-                print(f"Mario esta no chao? : {self.esta_no_chao(getState(ram, radius)[0], radius)}")
-                self.printState(getState(ram, radius)[0], radius)
-                print(150 * "-")
+                # print(f"posicao mario: ({x}, {y})")
+                # print(f"Mario esta no chao? : {self.mario_esta_no_chao(getState(ram, radius)[0], radius)}")
+                # self.printState(getState(ram, radius)[0], radius)
+                # print(150 * "-")
                 # Executa ação no jogo
                 rw, info = utils.performAction(acoes[indice_acao].codigo, env)
                 env.render()
                 ram_novo_estado = self.getRam(env)
                 new_state, new_x, new_y = getState(ram_novo_estado, radius=radius)
-                collision = False or ram_novo_estado[0x71]
+                collision = self.mario_esta_morto(ram_novo_estado)
+
+                if not self.mario_esta_no_chao(getState(ram_novo_estado, radius)[0], radius) and not collision:
+                    info, collision = self.espera_mario_voltar_chao(env, radius)
+                    ram_novo_estado = self.getRam(env)
+                    new_state, new_x, new_y = getState(ram_novo_estado, radius=radius)
 
                 # Calcula recompensa
                 reward = compute_reward(x, y, new_x, new_y, collision, info)
@@ -67,20 +72,29 @@ class QLearningMario:
 
                 # print( f"Adicionei o valor {q_table.retorna_acoes_estado(ram, step)} em qtable[{step}][{q_table.cria_chave(ram, step)}]")
 
-                ram, state, x, y = ram_novo_estado, new_state, new_x, new_y
+                if step > maximo_passos:
+                    maximo_passos = step
+                if x > pos_x:
+                    pos_x = x
+
                 # Verifica término do episódio
                 if collision:
                     print(f"Episódio {episode + 1}: Mario morreu em {step + 1} passos.")
+                    print(f"A ultima ação antes de morrer foi {acoes[indice_acao]} recebeu a recompensa {reward}")
+                    print(f"A ultima linha da tabela ficou: {q_table.retorna_acoes_estado(ram, step)} em qtable[{step}][{q_table.cria_chave(ram, step)}]")
                     print(f"{100 * '#'}")
+
                     break
                 elif step == max_steps_per_episode - 1:
                     print(f"Episódio {episode + 1}: Mario sobreviveu ao limite de passos.")
-                    return
-                if new_x >= 5014 or new_x == 0:
+                    return pos_x, step
+                if new_x >= 4800 or new_x == 0:
                     print(
                         f"Mario concluiu a fase com {step} iterações, após {episode} tentativas. posX é {new_x}; A tabela final é:")
                     env.close()
-                    return
+                    return pos_x, step
+                ram, state, x, y = ram_novo_estado, new_state, new_x, new_y
+        return pos_x, step
 
     def getStateMatrix(self, state, radius):
         state_n = np.reshape(state.split(','), (2 * radius + 1, 2 * radius + 1))
@@ -104,7 +118,7 @@ class QLearningMario:
                 line[radius] = 'X'
             print(line)
 
-    def esta_no_chao(self, state, radius):
+    def mario_esta_no_chao(self, state, radius) -> bool:
         matrix = self.getStateMatrix(state, radius)
         for i in range(len(matrix)):
             if 'X' in matrix[i]:
@@ -112,8 +126,24 @@ class QLearningMario:
                 return matrix[i + 1][indice_mario] == '$$'
         return False
 
+    def espera_mario_voltar_chao(self, env, radius) -> (any, bool):
+        rw, info = None, None
+        # print(f"{35*'#'} Mario Pulou {35*'#'}")
+        while not self.mario_esta_no_chao(getState(self.getRam(env), radius)[0], radius):
+            # print("Esperando mario voltar pro chao")
+            # self.printState(getState(self.getRam(env), radius)[0], radius)
+            # print()
+            rw, info = utils.performAction(131, env)
+            rw, info = utils.performAction(1, env)
+            env.render()
+            if self.mario_esta_morto(self.getRam(env)): return info, True
+        return info, False
+
     def getRam(self, env):
         ram = []
         for k, v in env.data.memory.blocks.items():
             ram += list(v)
         return np.array(ram)
+
+    def mario_esta_morto(self, ram) -> bool:
+        return False or ram[0x71]
