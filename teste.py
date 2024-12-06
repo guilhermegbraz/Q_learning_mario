1098c9cee2af81d2aa7f4914683daef787e4346b
Guilherme <guilherme.g.braz@outlook.com>
2024-12-06 00:12:18 -0300

    Alteração quantidade de ações + alteração nas recompensas

--- teste.py
+++ teste.py
@@ -3,16 +3,21 @@ from q_learning.q_table.QTableDicionarioPosicaoStep import QTableDicionarioPosic
 from q_learning.AcaoMario import AcaoMario
 from q_learning.q_table.QTableDicionarioState import QTableDicionarioState
 
+import os
+os.environ['TERM'] = 'xterm'
 
 q_table = QTableDicionarioPosicaoStep()
-q_table_state = QTableDicionarioState()
+q_table = QTableDicionarioState()
 actions_map = {
-    'runright': 130, 'runjumpright': 131,
-    'right': 128,
-    'down': 32, 'jump': 1,
+    'runright': 130,
+    'runjumpright': 131,
+    # 'right': 128,
+    # 'down': 32,
+    # 'jump': 1,
     'left': 64,
-    'jumpright': 129,
-    'spin': 256, 'spinright': 384
+    # 'jumpright': 129,
+    # 'spin': 256,
+    'spinright': 384
 }
 
 def compute_reward(old_x, old_y, new_x, new_y, collision, info):
@@ -20,7 +25,7 @@ def compute_reward(old_x, old_y, new_x, new_y, collision, info):
     if collision:
         return -100
     if new_x > old_x: recompensa += 500
-    if new_x < old_x: recompensa -= 50
+    if new_x < old_x: recompensa -= 5
     if new_y > old_y: recompensa += 30
     if new_y < old_y: recompensa -= 10
     if new_x == old_x: recompensa -= 500
@@ -31,8 +36,8 @@ def compute_reward(old_x, old_y, new_x, new_y, collision, info):
 def recompensa_x(old_x, old_y, new_x, new_y, collision, info) -> int:
     recompensa = 0
     if collision and new_y > old_y:
-        return -100
-    if collision and new_y < old_y: return 0
+        return 0
+    if collision and new_y < old_y: return -100
     if collision: return -100
     if new_y > 354: return 0
     if new_x > old_x: recompensa += 5 + new_x - old_x
@@ -43,9 +48,11 @@ def recompensa_x(old_x, old_y, new_x, new_y, collision, info) -> int:
 
 acoes = [AcaoMario(descricao, codigo) for descricao, codigo in actions_map.items()]
 q = QLearningMario("YoshiIsland1")
-q.treinar(
-    q_table_state, 0.7, 0.95, 0.3, 1000, 3000, acoes, recompensa_x
+maximo_x, maximo_passo = q.treinar(
+    q_table, 0.7, 0.95, 0.3, 250, 3000, acoes, recompensa_x
 )
+
+print(f"Ao final, mario atingiu a posx: {maximo_x}, e o maximo de passos foram {maximo_passo}")
 
 with open("q_table_dicionario_state.txt", 'w') as f:
     linhas_resultado = q_table.to_string()
